dc36702799bd9bf8b6499be2ed44cc812ab602c4
Youngsun Noh <youngsunkr@gmail.com>
2018-11-07 19:31:45 +0900

    Lazy loading test

---
+++ DeferredLoadingEnabled/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredLoadingEnabled
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            IEnumerable<Entity> entity = new IEnumerable<Entity>();
+            entity.authors[1].
+            //entity[0].au_fname.au_fname;
+
+            var items = (from x in entity.authors
+
+                         where x.au_fname.StartsWith(fname)
+
+                         select x).Take(2);
+
+
+
+            Console.WriteLine("SubSet 탐색 – 명지적 로딩");
+
+            foreach (authors item in items)
+
+            {
+
+                if (item.au_id == "213-46-8915")
+
+                {
+
+                    if (!item.titleauthor.IsLoaded)
+
+                        item.titleauthor.Load();
+
+
+
+                    foreach (var titleAu in item.titleauthor)
+
+                    {
+
+                        if (titleAu != null)
+
+                            Console.WriteLine("{0} : {1} : {2}"
+
+                                , titleAu.authors.au_fname, titleAu.au_id, titleAu.au_ord);
+
+                    }
+
+                }
+
+                else
+
+                {
+
+                    foreach (var titleAu in item.titleauthor)
+
+                    {
+
+                        if (titleAu != null)
+
+                            Console.WriteLine("{0} : {1} : {2}"
+
+                                , titleAu.authors.au_fname, titleAu.au_id, titleAu.au_ord);
+
+                    }
+
+                }
+
+            }
+            */
+
+        }
+    }
+
+    public class Entity
+    {
+        public string authors;
+        public string au_fname = string.Empty;
+    }
+}
--- Lazy Execution/Program.cs
+++ Lazy Execution/Program.cs
@@ -8,8 +8,30 @@ namespace Lazy_Execution
 {
     class Program
     {
+        //http://blog.naver.com/dotnethelper/60104797637
+        // Linq Execution 방식
+
+        /// <summary>
+        /// 연산되는 factor 변수의 값은 10일까? 20일까? LINQ쿼리의 실제적인 실행은 선안하는 시점이 아니라 루프문(foreach, while, for 등)이 실행되는 시점에 비로서 처리가 일어 나게 된다
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            List<int> number = new List<int> {1, 2, 3, 4, 5};
+
+            int factor = 10;
+
+            var result = from x in number
+                select x * factor;
+
+            factor = 20;
+
+            foreach (var item in result)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.ReadKey();
         }
     }
 }
